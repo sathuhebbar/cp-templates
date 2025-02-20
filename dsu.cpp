7aaf088d152efa1e5a1b0a7a7c03e7a30975d375
Sathvik Hebbar <sathvik@localhost.localdomain>
2025-02-20 23:19:53 +0530

    Add dsu template

---
+++ dsu.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class dsu {
+    int n;
+    vector<int> parent, rank;
+
+public:
+    dsu(int n) {
+        assert(n > 0);
+        this->n = n;
+        parent.resize(n);
+        rank.resize(n);
+
+        iota(parent.begin(), parent.end(), 0);
+        fill(rank.begin(), rank.end(), 1);
+    }
+
+    int find_set(int v) {
+        assert(v >= 0 and v < n);
+        if (parent[v] == v)
+            return v;
+        return parent[v] = find_set(parent[v]);
+    }
+
+    void union_sets(int u, int v) {
+        assert(u >= 0 and u < n);
+        assert(v >= 0 and v < n);
+        u = find_set(u);
+        v = find_set(v);
+
+        if (u != v) {
+            if (rank[v] < rank[u])
+                swap(u, v);
+            
+            parent[u] = v;
+            if (rank[u] == rank[v])
+                rank[v]++;
+        }
+    }
+};
+
+int main() {
+    dsu d(10);
+
+    d.union_sets(1, 3);
+    d.union_sets(5, 7);
+    d.union_sets(4, 6);
+    d.union_sets(6, 8);
+    d.union_sets(9, 1);
+    d.union_sets(4, 9);
+
+    cout << d.find_set(2) << '\n';
+    cout << d.find_set(3) << '\n';
+    cout << d.find_set(9) << '\n';
+    cout << d.find_set(5) << '\n';
+    return 0;
+}
